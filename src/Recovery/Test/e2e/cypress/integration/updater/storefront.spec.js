ad5f981e793d121ba2c6ee0dcaab5d994383992d
s.sluiter <s.sluiter@shopware.com>
2020-06-29 18:29:35 +0200

    ntr - test

--- src/Recovery/Test/e2e/cypress/integration/updater/storefront.spec.js
+++ src/Recovery/Test/e2e/cypress/integration/updater/storefront.spec.js
@@ -9,69 +9,70 @@ describe('Storefront test data set', () => {
      */
     it('@storefront: top navigation', () => {
         cy.visit('/');
-
+        cy.screenshot();
         cy.get('.nav > [href="/"]').should('be.visible');
-        cy.get('.nav > [href$="/Frauen/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Maenner/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Accessoires/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Angebote/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Unsere-besten-Produkte/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Fairtrade/"]').should('be.visible').click();
-        cy.get('.nav > [href$="/Angebote/"]').click()
-
-        cy.get('.home-link').click()
+        cy.screenshot();
+        cy.get('.nav > [href$="/Frauen/"]').should('be.visible').click();cy.screenshot();
+        cy.get('.nav > [href$="/Maenner/"]').should('be.visible').click();cy.screenshot();
+        cy.get('.nav > [href$="/Accessoires/"]').should('be.visible').click();cy.screenshot();
+        cy.get('.nav > [href$="/Angebote/"]').should('be.visible').click();cy.screenshot();
+        //cy.get('.nav > [href$="/Unsere-besten-Produkte/"]').should('be.visible').click();cy.screenshot();
+        cy.get('.nav > [href$="/Fairtrade/"]').should('be.visible').click();cy.screenshot();
+        cy.get('.nav > [href$="/Angebote/"]').click();cy.screenshot();
+        cy.screenshot();
+        cy.get('.home-link').click();cy.screenshot();
     });
 
     it('@storefront: put cargohose from cms page into cart', () => {
-        cy.visit('/Maenner/Hosen/');
+        cy.visit('/Maenner/Hosen/');cy.screenshot();
 
-        cy.get('.js-cookie-configuration-button > .btn').should('be.visible').click();
-        cy.get('.offcanvas-cookie > .btn').scrollIntoView().should('be.visible').click();
+        cy.get('.js-cookie-configuration-button > .btn').should('be.visible').click();cy.screenshot();
+        cy.get('.offcanvas-cookie > .btn').scrollIntoView().should('be.visible').click();cy.screenshot();
 
         cy.get('.cms-element-text > h2')
             .contains('Auf der Suche nach einer neuen Hose?')
             .should('be.visible');
+        cy.screenshot();
+        cy.get('.product-info').contains('Cargo').should('be.visible');cy.screenshot();
+        cy.get('.buy-widget > .btn').should('be.visible').click();cy.screenshot();
 
-        cy.get('.product-info').contains('Cargo').should('be.visible');
-        cy.get('.buy-widget > .btn').should('be.visible').click();
+        cy.get('.offcanvas').should('be.visible');cy.screenshot();
+        cy.get('.cart-item-label').contains(/1x.*Cargo/).should('be.visible');cy.screenshot();
 
-        cy.get('.offcanvas').should('be.visible');
-        cy.get('.cart-item-label').contains(/1x.*Cargo/).should('be.visible');
-
-        cy.get('.cart-item-remove > .btn').click();
-        cy.get('.cart-item-label').should('not.be.visible');
+        cy.get('.cart-item-remove > .btn').click();cy.screenshot();
+        cy.get('.cart-item-label').should('not.be.visible');cy.screenshot();
         cy.get('.alert-info > .alert-content-container > .alert-content')
             .contains('Warenkorb ist leer')
             .should('be.visible');
+        
+        cy.get('.offcanvas').scrollIntoView();cy.screenshot();
 
-        cy.get('.offcanvas').scrollIntoView()
-
-        cy.get('body > div.offcanvas.is-right.is-open > button').should('be.visible').click();
+        cy.get('body > div.offcanvas.is-right.is-open > button').should('be.visible').click();cy.screenshot();
         cy.get('.offcanvas').should('not.be.visible');
     });
 
     it('@storefront: search cargohose', () => {
-        cy.visit('/');
-        cy.get('input[type="search"]').should('be.visible').clear().type('cargohose{enter}');
+        cy.visit('/');cy.screenshot();
+        cy.get('input[type="search"]').should('be.visible').clear().type('cargohose{enter}');cy.screenshot();
 
-        cy.get('.search-headline').contains('cargohose').should('be.visible');
-        cy.get('.search-headline').contains('1 Produkt').should('be.visible');
+        cy.get('.search-headline').contains('cargohose').should('be.visible');cy.screenshot();
+        cy.get('.search-headline').contains('1 Produkt').should('be.visible');cy.screenshot();
 
-        cy.get('.product-info').contains('Cargo').should('be.visible');
+        cy.get('.product-info').contains('Cargo').should('be.visible');cy.screenshot();
     });
 
     it('@storefront: search cargohose downarrow enter', () => {
-        cy.visit('/');
+        cy.visit('/');cy.screenshot();
 
-        cy.get('input[type="search"]').should('be.visible').clear().type('cargohose');
-        cy.get('.search-suggest-product-link').should('be.visible');
-        cy.get('input[type="search"]').type('{downarrow}{enter}');
+        cy.get('input[type="search"]').should('be.visible').clear().type('cargohose');cy.screenshot();
+        cy.get('.search-suggest-product-link').should('be.visible');cy.screenshot();
+        cy.get('input[type="search"]').type('{downarrow}{enter}');cy.screenshot();
 
-        cy.get('.product-detail-name').contains('Cargohose').should('be.visible');
+        cy.get('.product-detail-name').contains('Cargohose').should('be.visible');cy.screenshot();
         cy.get('.h3.product-detail-description-title')
-            .contains(/Produktinformationen.*Cargohose/)
-            .should('be.visible')
-
-        cy.get('.btn-buy').should('be.visible');
+          .contains(/Produktinformationen.*Cargohose/)
+          .should('be.visible');
+        cy.screenshot();
+        cy.get('.btn-buy').should('be.visible');cy.screenshot();
     });
 });
